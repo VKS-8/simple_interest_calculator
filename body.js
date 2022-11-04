f65c14c7e9e4bad0d4170c9c2c40e10f7dd3440a
VK Johnson <64655526+VKS-8@users.nonreply.github.com>
2022-11-04 00:51:56 -0500

    Updated code, have a semi-working results paragraph, closer styling per the document design

--- body.js
+++ body.js
@@ -1,14 +1,48 @@
 
 const principal = document.getElementById('principal').value;
+const rate = document.getElementById('rate').value;
 const years = document.getElementById('years').value;
+const rate_val = document.getElementById('rate_val').value;
+const interest = document.getElementById('interest').value;
 
+// Sets current year to output correct ending year based on no. of years input
 let year = new Date().getFullYear()+parseInt(years);
 
+// Trying to use to update the interest rate amount based on slider input, but.....
+function updateRate(rate_val) {
+    let rateVal = document.getElementById('rate').value; 
+    document.getElementById('rate_val').innerHTML= `${rateVal}%`;
+}
 
+// When the 'Compute Interest' button is clicked, the updateRate Function is called which triggers the 
+// code that follows, uses capture instead of bubbling
+btn.addEventListener('click', updateRate, false);
 
-btn.addEventListener('click', compute, false);
+// The next three code blocks work together
+// Created new CustomEvent, does not allow bubbling,  
+// sets 'rate_val'to new output value in the 'resultStr' variable....hopefully;
+// Sourced from: https://developer.mozilla.org/en-US/docs/Web/Events/Creating_and_triggering_events
+const newRate_Val = new CustomEvent('updateRate', {
+    bubbles: false,
+    detail: { text: () => rate_val.value }
+  });
 
-const resultStr = `If you deposit $<span><mark>${principal}</mark></span>, `;
+// Form listens for the custom 'newRat_Val';
+// Sourced from: https://developer.mozilla.org/en-US/docs/Web/Events/Creating_and_triggering_events
+  form.addEventListener('newRate_Val', (e) => 
+  (e.detail.updateRate()));
+
+// As the range slider is moved, triggers the event to fire;
+// Sourced from: https://developer.mozilla.org/en-US/docs/Web/Events/Creating_and_triggering_events
+rate_val.addEventListener('input', (e) =>
+e.target.dispatchEvent(eventNewRate_Val));
+
+const resultStr = 
+`If you deposit $<span><mark>${principal}</mark></span> \<br\>
+at an interest rate of <span><mark>${newRate_Val}%</mark></span>, \<br\>
+you will receive an amount of $<span><mark>${interest}</mark></span> \<br\>
+in the year <span><mark>${year}</mark></span> \<b\>` 
+;
 
 document.getElementById('result').innerHTML= resultStr;
 
--- head.js
+++ head.js
@@ -1,25 +1,25 @@
 document.addEventListener("DOMContentLoaded", ready);
 
+// Computes the interest, changes the value in the 'Interest' output field, stops bubbling 
 function compute(e)
 {
     const principal = document.getElementById('principal').value;
     const rate = document.getElementById('rate').value;
     const years = document.getElementById('years').value;
 
-    
+ 
     let interest = principal * years * rate /100;
         document.getElementById('interest').innerHTML = '$'+interest.toFixed(2);
         e.stopPropagation;
     
-    
 }
-
+// Gets new value from the range slider and updates the number in the #rate_val span
 function updateRate(rate_val) {
     let rateVal = document.getElementById('rate').value; 
     document.getElementById('rate_val').innerHTML= `${rateVal}%`;
 }
 
-
+// Listens for the 'computer interest' button to be clicked then runs the compute function
 btn.addEventListener('click', compute, false);
 
 
