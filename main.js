dfe0da4f06b4f9824a5d72beb13cda64481a6a84
Veronica Johnson <64655526+VKS-8@users.noreply.github.com>
2023-05-29 21:10:23 -0500

    Add form event prevent default

--- main.js
+++ main.js
@@ -2,58 +2,66 @@
 
 // Get input values from HTML
 const getForm = document.querySelector('#form');
- console.log('Var getForm: ' + getForm);
+//  console.log('Var getForm: ' + getForm);
 const inputField = document.querySelector('input');
- console.log('Var inputField: ' + inputField);
+//  console.log('Var inputField: ' + inputField);
 const submitBtn = document.querySelector('#btn');
- console.log('Var submitBtn: ' + submitBtn);
+//  console.log('Var submitBtn: ' + submitBtn);
 
 let displayResults = document.querySelector('#result');
- console.log('Var displayResults: ' + displayResults);
+//  console.log('Var displayResults: ' + displayResults);
 
 // Declare variable to set the results to for display
 let resultStr;
- console.log('Var resultStr: ' + resultStr);
+//  console.log('Var resultStr: ' + resultStr);
 
+// Submit form event
 submitBtn.addEventListener('click', compute);
+// console.log(submitBtn);
+
+// Prevent default on form
+getForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
 
 // Gets new value from the range slider and updates the number in the #rate_val span,
 function updateRate() {
-    console.log('update() entry');
+    // console.log('update() entry');
     let rateVal = document.getElementById('rate').value; 
-    console.log('Var rateVal: ' + rateVal);
+    // console.log('Var rateVal: ' + rateVal);
     document.getElementById('rate_val').innerHTML= `${rateVal}%`;
-    console.log(`set innerHTML to ${rateVal}`);
+    // console.log(`set innerHTML to ${rateVal}`);
 }
 
 // Computes the interest, changes the value in the 'Interest' output field, 
 function compute(e) {   
-    e.preventDefault();  
-    console.log('compute() entry');
+    // console.log('compute() entry');
+    e.preventDefault(); 
+    // console.log(e);
     const principal = document.getElementById('principal').value;
-    console.log('Var principal: ' + principal);
+    // console.log('Var principal: ' + principal);
     const rate = document.getElementById('rate').value;
-    console.log('Var rate: ' + rate);
+    // console.log('Var rate: ' + rate);
     const years = document.getElementById('years').value;
-    console.log('Var years: ' + years);
+    // console.log('Var years: ' + years);
 
     validation();     
     console.log('validation() ran');
 
     let interest = principal * years * rate /100;
         document.getElementById('interest').innerHTML = '$'+interest.toFixed(2);
-        console.log('Var interest: ' + interest);
+        // console.log('Var interest: ' + interest);
     let year = new Date().getFullYear() + parseInt(years);
-        console.log('Var year: ' + year);
+        // console.log('Var year: ' + year);
 
     resultStr = 
         `If you deposit $<span class="resultStr"><mark>${principal}</mark></span> \<br\>
         at an interest rate of <span class="resultStr"><mark>${rate}%</mark></span>, \<br\>
         you will receive an amount of $<span class="resultStr"><mark>${interest.toFixed(2)}</mark></span> \<br\>
         in the year <span class="resultStr"><mark>${year}</mark></span>. \<b\>`;  
-            console.log('Var resultStr: ' + resultStr);
+            // console.log('Var resultStr: ' + resultStr);
     results();
-    console.log('results() ran');
+    // console.log('results() ran');
     // getForm.reset(); // Resets the form; but doesn't reset the range slider
     setTimeout(() => getForm.reset(), 5000);
     setTimeout(() => resetRangeSlider(), 5000);
@@ -68,13 +76,14 @@ function validation() {
     // and repurposed to meet current coding needs
     principalInput.addEventListener('input', () => {
         console.log('validation() handler entry');
-        // Clear all previous messages
-        principalInput.setCustomValidity('');
-        console.log('resets customValidity to empty');
+       
+        // // Clear all previous messages
+        // principalInput.setCustomValidity('');
+        // console.log('resets customValidity to empty');
         // Check validity of input field for principal
         principalInput.checkValidity();
         console.log(principalInput.checkValidity());
-        console.log('checkValidity() ran');       
+        console.log('checkValidity() ran'); 
     });
 
     // Listen for validity of principal input field
@@ -82,36 +91,24 @@ function validation() {
     // Prevent default form submission
     console.log('Invalid Check entry');
     principalInput.addEventListener('invalid', () => {
-            console.log('invalid')
+            console.log('invalid') 
+        
         principalInput.setCustomValidity(alert('Enter a positive number.'));
             console.log('validity alert ran');
+          
         }),
         console.log('Invalid Check ran');   
 }
-
-
-const nameField = document.querySelector("input");
-
-nameField.addEventListener("input", () => {
-  nameField.setCustomValidity("");
-  nameField.checkValidity();
-  console.log(nameField.checkValidity());
-});
-
-nameField.addEventListener("invalid", () => {
-  nameField.setCustomValidity("Please fill in your First Name.");
-});
 
 // Display results
 function results() {
     console.log('results() entry');
     // Set results to display in #result span
     displayResults.innerHTML = resultStr;
-    console.log(displayResults.innerHTML = resultStr);
+    // console.log(displayResults.innerHTML = resultStr);
 
     // Reset #result span to empty after 5 secs
     setTimeout(() => displayResults.innerHTML = '', 5000);
-    console.log('setTimeout complete');
     // Reset range slider text value to original value
     setTimeout(() => document.querySelector('#rate_val').innerHTML = '10.25%', 5000);
 }
@@ -121,4 +118,5 @@ function resetRangeSlider() {
     setTimeout(() => document.querySelector('#rate').value = 10.25, 5000);
     // document.querySelector('#rate_val').innerHTML = '10.25%';  For some reason this doesn't work here but it does in the results() function
     });
+    // console.log('setTimeout complete');
 }
